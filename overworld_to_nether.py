349786c2aba7b56b022563a3f405bd3a97151c93
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-06-30 00:16:36 +0530

    Enderpearl Saving and Chunk Loader trick!!

---
+++ overworld_to_nether.py
@@ -0,0 +1,16 @@
+x = int(input("Enter overworld X:"))
+Z = int(input("Enter overworld Z:"))
+
+nX = x/8
+nZ = Z/8
+
+print("[USELESS]Expected nether(initial) X:",nX)
+print("[USELESS]Expected nether Z(initial):",nZ)
+
+
+print("Final Nether X:",x)
+print("Final Nether Z:",Z)
+
+
+print("Nether equivalent overworld X:",x*8)
+print("Nether equivalent overworld Z:",Z*8)
